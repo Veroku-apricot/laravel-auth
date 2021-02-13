1d8ebddcb9eae4be83b269d7ffe324fe86c3effb
Veronika <veronikakuchtova95@gmail.com>
2021-02-13 13:17:13 +0100

    Store Img and change name

--- myProjectAuth/app/Http/Controllers/HomeController.php
+++ myProjectAuth/app/Http/Controllers/HomeController.php
@@ -6,23 +6,31 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
+    }
+
+    public function updateIcon(Request $request) {
+
+      $request -> validate([
+        'icon' => 'required|file'
+      ]);
+      $image = $request -> file('icon');
+
+      $ext = $image -> getClientOriginalExtension();
+      $name = rand(100000, 999999) . '_' . time();
+      $destFile = $name . '.' . $ext;
+
+      $file = $image -> storeAs('icon', $destFile, 'public');
+
+      dd($image, $destFile);
+
     }
 }
